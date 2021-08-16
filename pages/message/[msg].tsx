4dce45d0f5f9a176c2dcd5075dc3a91d25051306
sinsinpurin <tusanch6@gmail.com>
2021-08-17 04:14:37 +0900

    いろいろadd

--- pages/message/[msg].tsx
+++ pages/message/[msg].tsx
@@ -1,11 +1,18 @@
 import { useRouter } from "next/router"
+import styles from "styles/page/message.module.scss"
 
 const Message = ()=>{
     const router = useRouter()
     let { msg } = router.query
     return (
-        <div className="message">
-            {msg}
+        <div className={styles.messagepage}>
+            <div className={styles.container}>
+                <div className={styles.balloon}>
+                    <div className={styles.message}>
+                        {msg}
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
--- pages/qr/[msg].tsx
+++ pages/qr/[msg].tsx
@@ -1,14 +1,40 @@
 import { useRouter } from "next/router"
 import QRCode from "qrcode.react"
+import Footer from "components/footer"
+
+import styles from "styles/page/qr.module.scss"
 
 const QR = ()=>{
     const router = useRouter()
     let { msg } = router.query
     let url = "https://qr-messenger.vercel.app/message/" + msg
+
+    const QRDownload = ()=>{
+        const canvas = document.getElementById("qr-gen") as HTMLCanvasElement;
+        const pngUrl = canvas
+            .toDataURL("image/png")
+            .replace("image/png", "image/octet-stream");
+
+        let downloadLink = document.createElement("a");
+        downloadLink.href = pngUrl;
+        downloadLink.download = `QRMessage.png`;
+        document.body.appendChild(downloadLink);
+        downloadLink.click();
+        document.body.removeChild(downloadLink);
+    }
+
     return (
-        <>
-        <QRCode value={url}/>
-        </>
+        <div className={styles.qrpage}>
+            <div className={styles.container}>
+                <h1>QR Messenger</h1>
+                <div className={styles.showmsg}>Message: {msg}</div>
+                <div className={styles.qr}>
+                    <QRCode id="qr-gen" value={url}/>
+                </div>
+                <button className={styles.downloadbutton} onClick={QRDownload}>QRコード　ダウンロード</button>
+            </div>
+            <Footer/>
+        </div>
     )
 }
 
